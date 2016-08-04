a01f22a0a53cc869e8f9419d8d435b25acb045be
LiNk-NY <mramos09@gmail.com>
2016-08-04 17:51:22 -0400

    Minor changes

--- R/gatherCellLines.R
+++ R/gatherCellLines.R
@@ -1,15 +1,27 @@
 library(readr)
 library(dplyr)
 library(tidyr)
+
 matchcell <- read_csv("https://raw.githubusercontent.com/bhklab/DRUGNET/master/Curation/matching_cell.csv")
-## remove NA column
+
+## Find NA
+nacol <- which(is.na(names(matchcell)))
+if (length(nacol)) {
 matchcell <- matchcell[, -which(is.na(names(matchcell)))]
+}
+
+## Remove empty variable
+matchcell <- matchcell[, !names(matchcell) == "X14"]
+
+## remove NA column
 ## subset variables that are not ID vars into object
 verbosevars <- select(matchcell, Comment, COSMIC.tissueid)
+
 ## remove unwanted variables from data
 matchcell <- select(matchcell, -Comment, -COSMIC.tissueid)
 
 matchcell2 <- gather(matchcell, key = "idtype", value = "multi.cellid", 2:length(matchcell))
 matchcell2 %>% select(unique.cellid, multi.cellid) -> matchcell2
+
 matchcell2 <-  matchcell2[apply(matchcell2, 1, function(rows) { !duplicated(rows) })[2, ] ,]
 matchcellF <- matchcell2[complete.cases(matchcell2),]

1e1902d4f26423ec823e5709e043823c65546b71
LiNk-NY <mramos09@gmail.com>
2016-08-24 16:01:55 -0400

    Edit resulting tibble of CellLine IDs

--- R/gatherCellLines.R
+++ R/gatherCellLines.R
@@ -2,7 +2,8 @@ library(readr)
 library(dplyr)
 library(tidyr)
 
-matchcell <- read_csv("https://raw.githubusercontent.com/bhklab/DRUGNET/master/Curation/matching_cell.csv")
+# matchcell <- read_csv("https://raw.githubusercontent.com/bhklab/DRUGNET/master/Curation/matching_cell.csv")
+matchcell <- read_csv("./data/matching_cell.csv")
 
 ## Find NA
 nacol <- which(is.na(names(matchcell)))
@@ -22,6 +23,7 @@ matchcell <- select(matchcell, -Comment, -COSMIC.tissueid)
 
 matchcell2 <- gather(matchcell, key = "idtype", value = "multi.cellid", 2:length(matchcell))
 matchcell2 %>% select(unique.cellid, multi.cellid) -> matchcell2
+matchcell2 <- matchcell2[!duplicated(matchcell2),]
 
-matchcell2 <-  matchcell2[apply(matchcell2, 1, function(rows) { !duplicated(rows) })[2, ] ,]
-matchcellF <- matchcell2[complete.cases(matchcell2),]
+# matchcell2 <-  matchcell2[apply(matchcell2, 1, function(rows) { !duplicated(rows) })[2, ] ,]
+# matchcellF <- matchcell2[complete.cases(matchcell2),]
